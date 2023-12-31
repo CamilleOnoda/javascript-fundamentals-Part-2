890ee6aa785aaa4bec441a53a86f7ed3bca9b83e
Camille Onoda <info@camilleonoda.com>
2024-01-01 08:37:31 +0900

    Practice multiline arrow function

--- script.js
+++ script.js
@@ -8,11 +8,22 @@ function add7(nb) {
 }
 console.log(add7(7))
 
-function multiply(nb1,nb2) {
-    return nb1 * nb2;
-}
-let result = multiply(20,4);
-alert(result)
+// Arrow function
+let sum = (a,b) => a + b;
+console.log(sum(5,20));
+
+let ask = (question, yes, no) => confirm(question) ? yes() : no();
+ask(
+    "Do you agree?",
+    () => console.log("You agreed"),
+    () => console.log("You interrupted the execution"),
+);
+
+//function multiply(nb1,nb2) {
+//    return nb1 * nb2;
+//}
+//let result = multiply(20,4);
+//alert(result)
 
 function capitalize(str) {
     return str.charAt(0).toUpperCase();
